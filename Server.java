23b750683e48e32fb74a19c4d14a3eab1dc3b61e
LupeHole finder <olupotdouglas@gmail.com>
2014-09-10 17:41:42 +0300

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,55 @@
+package executor;
+
+public class Server {
+	public String local_user, local_pass, local_url, remote_url, remote_user,
+			remote_pass;
+
+	public String getLocal_user() {
+		return local_user;
+	}
+
+	public void setLocal_user(String local_user) {
+		this.local_user = local_user;
+	}
+
+	public String getLocal_pass() {
+		return local_pass;
+	}
+
+	public void setLocal_pass(String local_pass) {
+		this.local_pass = local_pass;
+	}
+
+	public String getLocal_url() {
+		return local_url;
+	}
+
+	public void setLocal_url(String local_url) {
+		this.local_url = local_url;
+	}
+
+	public String getRemote_url() {
+		return remote_url;
+	}
+
+	public void setRemote_url(String remote_url) {
+		this.remote_url = remote_url;
+	}
+
+	public String getRemote_user() {
+		return remote_user;
+	}
+
+	public void setRemote_user(String remote_user) {
+		this.remote_user = remote_user;
+	}
+
+	public String getRemote_pass() {
+		return remote_pass;
+	}
+
+	public void setRemote_pass(String remote_pass) {
+		this.remote_pass = remote_pass;
+	}
+
+}
